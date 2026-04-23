0414b2e4dc855c9243916d80734c0e98970eeb65
Rovsen001 <rovshanai-bpa205@code.edu.az>
2026-04-23 20:39:54 +0400

    init

---
+++ MVCTASK/MVCTASK/Controllers/Home/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCTASK.Controllers.Home
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
