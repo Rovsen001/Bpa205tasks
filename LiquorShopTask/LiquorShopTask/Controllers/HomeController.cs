6d7ba9c6d1ec4fd132dc4da21d44da6f20fe1a35
Rovsen001 <rovshanai-bpa205@code.edu.az>
2026-04-26 16:12:17 +0400

    init

---
+++ LiquorShopTask/LiquorShopTask/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using LiquorShopTask.Models;
+using LiquorShopTask.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LiquorShopTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vm = new HomeVM
+            {
+                Products = new List<Product>
+        {
+            new Product { Name = "Bacardi 151", ImageUrl = "/assets/images/prod-1.jpg", Price = 49 },
+            new Product { Name = "Jim Beam", ImageUrl = "/assets/images/prod-2.jpg", Price = 60 },
+            new Product { Name = "Rum", ImageUrl = "/assets/images/prod-3.jpg", Price = 50 },
+            new Product { Name = "Tequila Gold", ImageUrl = "/assets/images/prod-4.jpg", Price = 70 },
+            new Product { Name = "Black Label", ImageUrl = "/assets/images/prod-5.jpg", Price = 80 },
+            new Product { Name = "Macallan", ImageUrl = "/assets/images/prod-6.jpg", Price = 120 },
+            new Product { Name = "Old Monk", ImageUrl = "/assets/images/prod-7.jpg", Price = 55 },
+            new Product { Name = "Jameson", ImageUrl = "/assets/images/prod-8.jpg", Price = 65 }
+        },
+
+                Sliders = new List<Slider>
+        {
+            new Slider { Title = "Enjoy Premium Taste", ImageUrl = "/assets/images/bg_1.jpg" }
+        }
+            };
+
+            return View(vm);
+        }
+    }
+}
---
+++ LiquorShopTask/LiquorShopTask/Models/ViewModels/HomeVM.cs
@@ -0,0 +1,8 @@
+﻿namespace LiquorShopTask.Models.ViewModels
+{
+    public class HomeVM
+    {
+        public List<Product> Products { get; set; }
+        public List<Slider> Sliders { get; set; }
+    }
+}
