1959e770556dfb0b26ea57a5bb9ff7273815436c
Rovsen001 <rovshanai-bpa205@code.edu.az>
2026-05-03 15:47:25 +0400

    init

---
+++ DigiMediaTask/DigiMediaTask/Areas/Admin/Controllers/ProjectController.cs
@@ -0,0 +1,48 @@
+﻿using DigiMediaTask.DAL;
+using DigiMediaTask.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DigiMediaTask.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProjectController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ProjectController(AppDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Project> Projects = _db.Projects.ToList();
+            return View(Projects);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Project Project)
+        {
+            _db.Projects.Add(Project);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            Project Project = _db.Projects.Find(id);
+            Project.IsDeleted = true;
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+        [HttpPost]
+        public IActionResult Restore(int id)
+        {
+            Project Project = _db.Projects.Find(id);
+            Project.IsDeleted = false;
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ DigiMediaTask/DigiMediaTask/Models/Project.cs
@@ -0,0 +1,11 @@
+﻿using DigiMediaTask.Models.Base;
+
+namespace DigiMediaTask.Models
+{
+    public class Project : BaseEntity
+    {
+        public string Profession { get; set; }
+        public string Category { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
