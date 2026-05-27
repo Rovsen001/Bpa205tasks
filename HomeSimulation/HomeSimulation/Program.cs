b631f5ecdb43005e4fa8495066df8a5f162b6730
Rovsen001 <rovshanai-bpa205@code.edu.az>
2026-05-28 02:07:44 +0400

    init

---
+++ HomeSimulation/HomeSimulation/Program.cs
@@ -0,0 +1,45 @@
+using HomeSimulation.DAL;
+using HomeSimulation.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HomeSimulation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<AppDbContext>(option =>
+            {
+                option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
+            builder.Services.AddIdentity<AppUser,IdentityRole>(option =>
+            {
+                option.Password.RequireNonAlphanumeric = true;
+                option.Password.RequireUppercase = true;
+                option.Password.RequireLowercase = true;
+                option.Password.RequireDigit = true;
+                option.Password.RequiredLength = 8;
+
+                option.User.RequireUniqueEmail = true;
+                option.User.AllowedUserNameCharacters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.@";
+            }).AddEntityFrameworkStores<AppDbContext>();
+
+
+            var app = builder.Build();
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{Id?}"
+                );
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{Id?}"
+                );
+            app.UseStaticFiles();
+            app.Run();
+        }
+    }
+}
---
+++ HomeSimulation/HomeSimulation/Utilities/Image/FileUploadExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace HomeSimulation.Utilities.Image
+{
+    public static class FileUploadExtensions
+    {
+        public static string SaveImage(this IFormFile file,IWebHostEnvironment env,string folder)
+        {
+            string path=Path.Combine(env.WebRootPath,folder);
+            string filename=Guid.NewGuid().ToString()+file.FileName;
+            string fullpath=Path.Combine(path,filename);
+
+            using(FileStream stream =new FileStream(path, FileMode.Create))
+            {
+                file.CopyTo(stream);
+            }
+            return filename;
+        }
+    }
+}
